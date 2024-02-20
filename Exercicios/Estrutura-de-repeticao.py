094c43662a1f69244874d282ab991813706927a2
Alvaro Ito <alvaro.e.ito@gmail.com>
2024-02-20 17:05:53 -0300

    atualizando curso

---
+++ Exercicios/Estrutura-de-repeticao.py
@@ -0,0 +1,286 @@
+#1. Faga um programa que receba dois números e mostre qual deles é o maior. 
+'''
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+if n1 > n2:
+    print(f'{n1} é maior que {n2}')
+elif n1 == n2:
+    print(f'{n1} e {n2} são iguais')
+else:
+    print(f'{n2} é maior que {n1}')
+'''
+
+#2. Leia um numero fornecido pelo usuário. Se esse número for positivo, calcule a raiz  quadrada do número. Se o número for negativo, mostre uma mensagem dizendo que o número é invalido. 
+'''
+from math import sqrt
+num = int(input('Digite um número: '))
+if num > 0:
+    print(f'A raiz quadrada de {num} é {sqrt(num):.2f}')
+else:
+    print(f'Número inválido!')
+'''
+
+#3. Leia um numero real. Se o número for positivo imprima a raiz quadrada. Do contrario, imprima o numero ao quadrado. 
+'''
+from math import sqrt
+num = int(input('Digite um número: '))
+if num > 0:
+    print(f'A raiz quadrada de {num} é {sqrt(num):.2f}')
+else:
+    print(f'O quadrado de {num} é {num ** 2}')
+'''
+
+#4. Faga um programa que leia um número e, caso ele seja positivo, calcule e mostre: 
+# O número digitado ao quadrado 
+# A raiz quadrada do número digitado 
+'''
+from math import sqrt
+num = int(input('Digite um número: '))
+if num > 0:
+    print(f'A raiz quadrada de {num} é {sqrt(num):.2f}')
+    print(f'O quadrado de {num} é {num ** 2}')
+else:
+    print(f'Número inválido!')
+'''
+
+#5. Faça um programa que receba um número inteiro e verifique se este nimero é par ou impar. 
+'''
+num = int(input('Digite um número: '))
+if num % 2 == 0:
+    print(f'{num} é par')
+else:
+    print(f'{num} é ímpar')
+'''
+
+#6. Escreva um programa que, dados dois números inteiros, mostre na tela o maior deles, assim como a diferenca existente entre ambos. 
+'''
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print(f'{n1} é maior que {n2}')
+    print(f'{n1} - {n2} = {n1 - n2}')
+elif n1 == n2:
+    print(f'{n1} e {n2} são iguais')
+    print(f'{n1} - {n2} = {n1 - n2}')
+else:
+    print(f'{n2} é maior que {n1}')
+    print(f'{n2} - {n1} = {n2 - n1}')
+'''
+#7. Faga um programa que receba dois números e mostre o maior. Se por acaso, os dois números forem iguais, imprima a mensagem Números iguais. 
+'''
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print(f'{n1} é maior que {n2}')
+elif n1 == n2:
+    print(f'{n1} e {n2} são iguais')
+else:
+    print(f'{n2} é maior que {n1}')
+'''
+
+#8. Faga um programa que leia 2 notas de um aluno, verifique se as notas sao validas e exiba na tela a média destas notas. Uma nota valida deve ser,  obrigatoriamente, um valor entre 0.0 e 10.0, onde caso a nota nao possua um valor valido, este fato deve ser informado ao usuario e o programa termina.
+'''
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+
+if n1 < 0 or n1 > 10 or n2 < 0 or n2 > 10:
+    print(f'Nota inválida')
+else:
+    print(f'Média: {(n1 + n2) / 2}')
+'''
+
+#9. Leia o saldrio de um trabalhador e o valor da prestação de um empréstimo. Se a prestação for maior que 20% do salario imprima: Empréstimo ndo concedido, caso contrério imprima: Empréstimo concedido.
+
+
+#10. Faga um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu peso ideal, utilizando as seguintes formulas (onde / corresponde a altura):
+# Homens: (72.7+ h) — 58 
+# Mulheres: (62,1 = h) — 44,7
+
+
+#11. Escreva um programa que leia um número inteiro maior do que zero e devolva, na tela, a soma de todos os seus algarismos. Por exemplo, ao número 251 corresponderá o valor 8(2+5+1). Se o número lido não for maior do que zero, o programa terminara com a mensagem “Número inválido”. 
+
+
+#12. Ler um número inteiro. Se o número lido for negativo, escreva a mensagem “Número invalido”. Se o número for positivo, calcular o logaritmo deste numero. 
+
+
+#13. Faga um algoritmo que calcule a média ponderada das notas de 3 provas. A primeira e a segunda prova tém peso 1 e a terceira tem peso 2. Ao final, mostrar a média do aluno e indicar se o aluno foi aprovado ou reprovado. A nota para aprovagao deve ser igual ou superior a 60 pontos. 
+
+
+#14. Anota final de um estudante é calculada a partir de trés notas atribuidas entre o intervalo de 0 até 10, respectivamente, a um trabalho de laboratério, a uma avaliagao semestral e a um exame final. A média das trés notas mencionadas anteriormente obedece aos pesos: Trabalho de Laboratério: 2; Avaliagao Semestral: 3; Exame Final: 5. De acordo com o resultado, mostre na tela se o aluno esta reprovado (média entre 0 e 2,9), de recuperacao (entre 3 e 4,9) ou se foi aprovado. Faça todas as verificagoes necessarias. 
+
+
+#15. Usando switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da semana correspondente a este numero. Isto é, domingo se 1, segunda -feira se 2, e assim por diante. 
+
+
+#16. Usando switch, escreva um programa que leia um inteiro entre 1 e 12 e imprima o mês correspondente a este numero. Isto é, janeiro se 1, fevereiro se 2, e assim por diante. 
+
+
+#17. Faça um programa que calcule e mostre a área de um trapézio. Sabe-se que: 
+#A = (basemaior + basemenor) * altura / 2
+#Lembre-se a base maior e a base menor devem ser números maiores que zero.
+
+
+#18. Faga um programa que mostre ao usuario um menu com 4 opgoes de operagoes matematicas (as basicas, por exemplo). O usudrio escolhe uma das opções e o seu programa então pede dois valores numéricos e realiza a operação, mostrando o resultado e saindo. 
+
+
+#19. Faga um programa para verificar se um determinado nimero inteiro e divisivel por 3 ou 5, mas nao simultaneamente pelos dois. 
+
+
+#20. Dados trés valores, A, B, C, verificar se eles podem ser valores dos lados de um tridngulo e, se forem, se é um tridngulo escaleno, equilatero ou iséscele, considerando os seguintes conceitos: 
+# O comprimento de cada lado de um triangulo é menor do que a soma dos outros dois lados. 
+# Chama-se equilatero o triangulo que tem trés lados iguais. 
+# Denominam-se isosceles o triangulo que tem o comprimento de dois lados iguais. 
+# Recebe o nome de escaleno o tridngulo que tem os trés lados diferentes. 
+
+
+#21. Escreva o menu de opções abaixo. Leia a opção do usuário e execute a operagao escolhida. Escreva uma mensagem de erro se a opgao for invalida. 
+    #Escolha a opção: 
+    #1- Soma de 2 números. 
+    #2- Diferença entre 2 números (maior pelo menor). 
+    #3- Produto entre 2 números. 
+    #4- Divisão entre 2 números (o denominador não pode ser zero). 
+    #Opção 
+
+
+#22. Leia a idade e o tempo de servico de um trabalhador e escreva se ele pode ou não se aposentar. As condições para aposentadoria são 
+# Ter pelo menos 65 anos, 
+# Ou ter trabalhado pelo menos 30 anos, 
+# Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.
+
+
+#23. Determine se um determinado ano lido é bissexto. Sendo que um ano é bissexto se for divisivel por 400 ou se for divisivel por 4 e nao for divisivel por 100. Por exemplo: 1988, 1992, 1996 
+
+
+#24. Uma empresa vende o mesmo produto para quatro diferentes estados. Cada estado possui uma taxa diferente de imposto sobre o produto (MG 7%; SP 12%; RJ 15%; MS 8%). Faga um programa em que o usudrio entre com o valor e o estado destino do produto e o programa retorne o preco final do produto acrescido do imposto do estado em que ele sera vendido. Se o estado digitado nao for valido, mostrar uma mensagem de erro. 
+
+
+#25. Calcule as raizes da equagao de 2° grau. 
+#Lembrando que: 
+#x = -b +- raiz de delta / 2*a
+#Onde 
+#Delta = b² - 4ac 
+#E ax² + bx + c = 0 representa uma equação de 2° grau. 
+#A variavel a tem que ser diferente de zero. Caso seja igual, imprima a mensagem “Nao é equacao de segundo grau”. 
+# Se A < 0, nao existe real. Imprima a mensagem Não existe raiz. 
+# Se A = (), existe uma raiz real. Imprima a raiz e a mensagem Raiz tunica. 
+# Se A > 0, imprima as duas raizes reais.
+
+
+#26. Leia a distancia em Km e a quantidade de litros de gasolina consumidos por um carro em um percurso, calcule o consumo em Km /I e escreva uma mensagem de acordo com a tabela abaixo: 
+    #CONSUMO | (Km/) MENSAGEM 
+    #menor que 8 Venda o carro! 
+    #entre 8 e l4 Econémico! 
+    #maior que 12 Super econdmico! 
+
+
+#27. Escreva um programa que, dada a idade de um nadador, classifique-o em uma das seguintes categorias: 
+    #Categoria | Idade 
+    #InfantilA | 5a7 
+    #InfantilB | 8a 10 
+    #JuvenilA | 11213 
+    #JuvenilB | 14 a 17 
+    #Sênior maiores de 18 anos 
+
+
+#28. Faça um programa que leia trés números inteiros positivos e efetue o cálculo de uma das seguintes médias de acordo com um valor numérico digitado pelo usuário: 
+#(a) Geométrica: raiz cubica de x * y * z 
+#(b) Ponderada: (x + 2*y+3*z) / 6
+#(c) Harmônica: 1((1/x + 1/y + 1/z))
+#(d) Aritmética: (x+y+z) / 3
+
+
+#29. Faça uma prova de matematica para criangas que estão aprendendo a somar números inteiros menores do que 100. Escolha números aleatérios entre 1 e 100, e mostre na tela a pergunta: qual é a soma de a + b, onde a e b são os nimeros aleatérios. Peça a resposta. Faca cinco perguntas ao aluno, e mostre para ele as perguntas e as respostas corretas, além de quantas vezes o aluno acertou. 
+
+
+#30. Faga um programa que receba trés números e mostre-os em ordem crescente. 
+
+
+#31. Fagaum programa que receba a altura e o peso de uma pessoa. De acordo com a tabela a seguir, verifique e mostra qual a classificagao dessa pessoa. 
+    #Altura         Peso 
+                    #Até 60 | Entre 60 e 90 (Inclusive) | Acima de 90 
+    #Menor que 1,20     A               D                   G 
+    #De 1,20 a1,70      B               E                   H 
+    #Maior que 1,70     c               F                   I
+
+
+#32. Escrever um programa que leia o cédigo do produto escolhido do cardapio de uma lanchonete e a quantidade. O programa deve calcular o valor a ser pago por aquele lanche. Considere que a cada execugao somente sera calculado um pedido. O cardapio da lanchonete segue o padrao abaixo: 
+#Especificação      Código       Preço 
+#Cachorro Quente    100          1.20 
+#Bauru Simples      101          1.30 
+#Bauru com Ovo      102          1.50 
+#Hamburguer         103          1.20 
+#Cheeseburguer      104          1.70 
+#Suco               105          2.20 
+#Refrigerante       106          1.00 
+
+
+#33. Um produto vai sofrer aumento de acordo com a tabela abaixo. Leia o preço antigo, calcule e escreva o preço novo, e escreva uma mensagem em função do preço novo (de acordo com a segunda tabela). 
+#PREÇO ANTIGO       PERCENTUAL DE AUMENTO         PREÇO NOVO                        MENSABEM 
+#até R$ 50                  5%                    até R$ 80                         Barato 
+#entre R$ 50 e R$ 100       10%                   entre R$ 80 e R$ 120 (inclusive)  Normal 
+#acima de R$ 100            15%                   entre R$ 120 e R$ 200 (inclusive) Caro 
+#                                                 acima de R$ 200                   Muito caro 
+
+
+#34. Leia a nota e o número de faltas de um aluno, e escreva seu conceito. De acordo com a tabela abaixo, quando o aluno tem mais de 20 faltas ocorre uma redução de conceito. 
+#NOTA           CONCEITO (ATE 20 FALTAS)        CONCEITO (MAIS DE 20 FALTAS) 
+#9.0 até 10.0               A                                 B
+#7.5 até 8.9                B                                 C
+#5.0 até 7.4                C                                 D
+#4.0 até 4.9                D                                 E
+#0.0 até 3.9                E                                 E
+
+
+#35. Leia uma data e determine se ela é valida. Ou seja, verifique se o més esta entre 1 e 12, e se o dia existe naquele més. Note que Fevereiro tem 29 dias em anos bissextos, e 28 dias em anos nao bissextos. 
+
+
+#36. Escreva um programa que, dado o valor da venda, imprima a comissão que deverá ser paga ao vendedor. Para calcular a comissão, considere a tabela abaixo: 
+#Venda mensal                                               Comissão 
+#Maior ou igual a R$100.000,00                              R$700,00 + 16% das vendas 
+#Menor que R$100.000,00 e maior ou igual a R$80.000,00      R$650,00 +14% das vendas 
+#Menor que R$80.000,00 e maior ou igual a R$60.000,00       R$600,00 +14% das vendas 
+#Menor que R$60.000,00 e maior ou igual a R$40.000,00       R$550,00 +14% das vendas 
+#Menor que R$40.000,00 e maior ou igual a R$20.000,00       R$500,00 +14% das vendas 
+#Menor que R$20.000,00                                      R$400,00 +14% das vendas 
+
+
+#37. As tarifas de certo parque de estacionamento são as seguintes: 
+# 1º e 2º hora - R$ 1,00 cada 
+# 3º e 4º hora - R$ 1,40 cada 
+# 5º hora e seguintes - R$ 2,00 cada 
+#O número de horas a pagar é sempre inteiro e arredondado por excesso. Deste modo, quem estacionar durante 61 minutos pagará por duas horas, que é o mesmo que pagaria se tivesse permanecido 120 minutos. Os momentos de chegada ao parque e partida deste são apresentados na forma de pares de inteiros, representando horas e minutos. Por exemplo, o par 12 50 representará “dez para a uma da tarde”. Pretende-se criar um programa que, lidos pelo teclado os momentos de chegada e de partida, escreva na tela o preço cobrado pelo estacionamento. Admite-se que a chegada e a partida se dão com intervalo não superior a 24 horas. Portanto, se uma dada hora de chegada for superior à da partida, isso não é uma situação de erro, antes significará que a partida ocorreu no dia seguinte ao da chegada. 
+
+
+#38. Leia uma data de nascimento de uma pessoa fornecida através de três números inteiros: 
+#Dia, Mês e Ano. Teste a validade desta data para saber se esta é uma data válida. Teste se o dia fornecido é um dia válido: dia > O, dia < 28 para o mês de fevereiro (29 se o ano for bissexto), dia < 30 em abril, junho, setembro e novembro, dia < 31 nos outros meses. Teste a validade do més: més > 0 e mês < 13. Teste a validade do ano: ano < ano atual (use uma constante definida com o valor igual a 2008). Imprimir: “data válida” ou “data inválida” no final da execução do programa.
+
+
+#39. Uma empresa decide dar um aumento aos seus funcionarios de acordo com uma tabela que considera o salario atual e o tempo de servico de cada funcionario. Os funcionarios com menor salario terao um aumento proporcionalmente maior do que os funcionarios com um salario maior, e conforme o tempo de servi¢o na empresa, cada funcionario irá receber um bônus adicional de salario. Faça um programa que leia: 
+# 0 valor do saldrio atual do funcionario; 
+# 0 tempo de servigo desse funciondrio na empresa (número de anos de trabalho na empresa). 
+#Use as tabelas abaixo para calcular o salario reajustado deste funcionario e imprima o valor do salério final reajustado, ou uma mensagem caso o funcionario nao tenha direito a nenhum aumento. 
+#Salario Atual      Reajuste(%)    Tempo de Servico    Bonus 
+#Até 500,00         25%            Abaixo de 1 ano     Sem bénus 
+#Até 1000,00        20%            De 1 a 3 anos       100,00 
+#Até 1500,00        15%            De 4 a 6 anos       200,00 
+#Até 2000,00        10%            De 7 a 10 anos      300,00 
+#Acima de 2000,00   Sem reajuste   Mais de 10 anos     500,00 
+
+
+#40. O custo ao consumidor de um carro novo é a soma do custo de fabrica, da comissao do distribuidor, e dos impostos. A comissao e os impostos sao calculados sobre o custo de fabrica, de acordo com a tabela abaixo. Leia o custo de fabrica e escreva o custo ao consumidor. 
+#CUSTO DE FABRICA               % DO DISTRIBUIDOR       % DOS IMPOSTOS 
+#até R$12.000,00                5                       isento 
+#entre R$12.000,00 e 25.000,00  10                      15 
+#acima de R$25.000,00           15                      20 
+
+
+#41. Faga um algoritmo que calcule o IMC de uma pessoa e mostre sua classificagao de acordo com a tabela abaixo: 
+#IMC            Classificagao 
+#<185           Abaixo do Peso 
+#18,6 - 24,9    Saudavel 
+#25,0 - 29,9    Peso em excesso 
+#30,0 - 34,9    Obesidade Grau I 
+#35,0 - 39,9    Obesidade Grau II(severa) 
+#> 40,0         Obesidade Grau III(mórbida)
