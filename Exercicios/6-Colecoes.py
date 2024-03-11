2b603b4e0c52472d33dd3e88370a46c619069bf5
Alvaro Ito <alvaro.e.ito@gmail.com>
2024-03-11 16:54:44 -0300

    atualizando

--- Exercicios/6-Colecoes.py
+++ Exercicios/6-Colecoes.py
@@ -351,24 +351,109 @@ for n in range(0, 5):
 print(produto)
 '''
 #24. Faça um programa que leia dez conjuntos de dois valores, o primeiro representando o número do aluno e o segundo representando a sua altura em metros. Encontre o aluno mais baixo e o mais alto. Mostre o nimero do aluno mais baixo e do mais alto, juntamente com suas alturas. 
+'''
+lista_num_alunos = []
+lista_alt_alunos = []
+for n in range(1, 4):
+    num_aluno = int(input('Digite o número do aluno: '))
+    lista_num_alunos.append(num_aluno)
+    alt_aluno = float(input('Digite a altura do aluno: '))
+    lista_alt_alunos.append(alt_aluno)
 
-#25. Faga um programa que preencha um vetor de tamanho 100 com os 100 primeiros naturais que nao sao múltiplos de 7 ou que terminam com 7. 
+#armazenar a altura do mais baixo e alto
+alt_baixo = min(lista_alt_alunos)
+alt_alto = max(lista_alt_alunos)
+#pegar indice do mais baixo e alto
+indice_baixo = lista_alt_alunos.index(alt_baixo)
+indice_alto = lista_alt_alunos.index(alt_alto)
+#pegar número do aluno pelo indice.
+num_baixo = lista_num_alunos[indice_baixo]
+num_alto = lista_num_alunos[indice_alto]
+
+print(f'Número do aluno mais baixo: {num_baixo} com {alt_baixo:.2f} mts')
+print(f'Número do aluno mais alto: {num_alto} com {alt_alto:.2f} mts')
+'''
+
+#25. Faga um programa que preencha um vetor de tamanho 100 com os 100 primeiros naturais que nao sao múltiplos de 7 ou que terminam com 7. 
+'''
+for n in range(1, 101):
+    if n % 7 == 0 or n % 10 == 7:
+        print(n)
+'''
 
 ###26. Faça um programa que calcule o desvio padrao de um vetor v contendo n = 10 números, onde m é a media do vetor. 
 
 #27. Leia 10 números inteiros e armazene em um vetor. Em seguida escreva os elementos que são primos e suas respectivas posições no vetor. 
 
 #28. Leia 10 numeros inteiros e armazene em um vetor v. Crie dois novos vetores v1 e v2. Copie os valores impares de v para v1, e os valores pares de v para v2. Note que cada um dos vetores v1 e v2 tém no maximo 10 elementos, mas nem todos os elementos sao utilizados. No final escreva os elementos UTILIZADOS de v1 e v2.
+'''
+lista_num = []
+lista_par = []
+lista_impar = []
+
+for n in range(1, 11):
+    num = int(input(f'Digite o {n}º número: '))
+    lista_num.append(num)
+    if num % 2 == 0:
+        lista_par.append(num)
+    else:
+        lista_impar.append(num)
+
+print(f'Listas de números digitados: {lista_num}')
+print(f'Listas de números pares digitados: {lista_par}')
+print(f'Listas de números impares digitados: {lista_impar}')
+'''
 
 #29. Faça um programa que receba 6 números inteiros e mostre: 
 # Os números pares digitados; 
 # A soma dos números pares digitados; 
 # Os números impares digitados; 
 # A quantidade de números impares digitados; 
+'''
+lista_num = []
+lista_par = []
+lista_impar = []
+for n in range(1, 7):
+    num = int(input('Digite um número: '))
+    lista_num.append(num)
+    if num % 2 == 0:
+        lista_par.append(num)
+    else:
+        lista_impar.append(num)
+
+print(f'Números digitados: {lista_num}')
+print(f'Números pares digitados: {lista_par}')
+print(f'Números impares digitados: {lista_impar}')
+print(f'Quantidade de números impares digitados: {len(lista_impar)}')
+'''
 
 #30. Faça um programa que leia dois vetores de 10 elementos. Crie um vetor que seja a intersecção entre os 2 vetores anteriores, ou seja, que contém apenas os números que estão em ambos os vetores. Não deve conter números repetidos. 
+'''
+lista_1 = []
+lista_2 = []
+for n in range(1, 11):
+    lista_1.append(int(input('Digite um número da primeira lista: ')))
+for n in range(1, 11):
+    lista_2.append(int(input('Digite um número da segunda lista: ')))
+conj_1 = set(lista_1)
+conj_2 = set(lista_2)
+interseccao = conj_1.intersection(conj_2)
+print(f'Números de intersecção entre os 2 vetores: {interseccao}')
+'''
 
-#31. Façaum programa que leia dois vetores de 10 elementos. Crie um vetor que seja a união entre os 2 vetores anteriores, ou seja, que contém os números dos dois vetores. Não deve conter números repetidos. 
+#31. Faça um programa que leia dois vetores de 10 elementos. Crie um vetor que seja a união entre os 2 vetores anteriores, ou seja, que contém os números dos dois vetores. Não deve conter números repetidos. 
+'''
+lista_1 = []
+lista_2 = []
+for n in range(1, 11):
+    lista_1.append(int(input('Digite um número da primeira lista: ')))
+for n in range(1, 11):
+    lista_2.append(int(input('Digite um número da segunda lista: ')))
+conj_1 = set(lista_1)
+conj_2 = set(lista_2)
+uniao = conj_1.union(conj_2)
+print(f'Números de intersecção entre os 2 vetores: {uniao}')
+'''
 
 #32. Leia dois vetores de inteiros x e y, cada um com 5 elementos (assuma que o usuário não informa elementos repetidos). Calcule e mostre os vetores resultantes em cada caso abaixo: 
 # Soma entre x e y: soma de cada elemento de = com o elemento da mesma posição em y. 
@@ -378,14 +463,42 @@ print(produto)
 # União entre = e y: todos os elementos de r, e todos os elementos de y que não estão em r.
 
 #33. Faça um programa que leia um vetor de 15 posições e o compacte, ou seja, elimine as posições com valor zero. Para isso, todos os elementos à frente do valor zero, devem ser movidos uma posição para trás no vetor. 
+'''
+lista_num = []
+lista_num_comp = []
+for n in range(1, 16):
+    num = int(input('Digite um número: '))
+    lista_num.append(num)
+    if num == 0:
+        pass
+    else:
+        lista_num_comp.append(num)
+
+print(lista_num)
+print(lista_num_comp)
+'''
 
 #34. Faça um programa para ler 10 números DIFERENTES a serem armazenados em um vetor. Os dados deverão ser armazenados no vetor na ordem que forem sendo lidos, sendo que caso o usuário digite um número que já foi digitado anteriormente, o programa deverá pedir para ele digitar outro número. Note que cada valor digitado pelo usuário deve ser pesquisado no vetor, verificando se ele existe entre os números que já foram fornecidos. Exibir na tela o vetor final que foi digitado. 
+'''
+lista_num = []
+while len(lista_num) < 16:
+    num = int(input('Digite um número: '))
+    if num in lista_num:
+        print(f'{num} já está na lista')
+    else:
+        lista_num.append(num)
+
+print(lista_num)
+'''
 
 #35. Faça um programa que leia dois números a e b (positivos menores que 10000) e: 
 # Crie um vetor onde cada posição é um algarismo do número. A primeira posição é o algarismo menos significativo; 
 # Crie um vetor que seja a soma de a e b, mas faça-o usando apenas os vetores construídos anteriormente. 
 #Dica: some as posições correspondentes. Se a soma ultrapassar 10, subtraia 10 do resultado e some 1 à próxima posição. 
 
+a = '1234'
+for l in a:
+    print(l)
 #36. Leia um vetor com 10 números reais, ordene os elementos deste vetor, e no final escreva os elementos do vetor ordenado.
 
 #37. Considere um vetor A com 11 elementos onde Al < A2 < ... < A6 > AT > A8 > ... > A11, ou seja, está ordenado em ordem crescente até o sexto elemento, e a partir desse elemento está ordenado em ordem decrescente. Dado o vetor da questao anterior, proponha um algoritmo para ordenar os elementos. 
